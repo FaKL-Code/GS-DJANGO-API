4710b5e1c782d1621478436674e3b27cb5cb5e45
Julio Facal <julio@facal.dev>
2023-05-31 22:15:38 -0300

    initial commit

---
+++ salgadelicia/fandangos/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+VOLUME = ((
+    ('PEQUENO', 'Pequeno'),
+    ('MEDIO', 'Médio'),
+    ('GRANDE', 'Grande'),
+))
+
+class Doador(models.Model):
+    nome = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=14)
+    email = models.EmailField(max_length=50)
+    telefone = models.CharField(max_length=15)
+    endereco = models.CharField(max_lenght=100)
+
+    def __str__(self):
+        return self.nome
+
+def MinValueValidator(data):
+    hoje = date.today()
+
+    if data < hoje:
+        raise ValidationError('Data de retirada não pode ser no passado')
+
+
+class Doacao(models.Model):
+    volume = models.ChoiceField(max_length=20, choices=VOLUME, null=False, blank=False)
+    data_doacao = models.DateField(auto_now_add=True)
+    data_retirada = models.DateField(validators=[MinValueValidator])
+
+
+
